82dab99f1d55db1074dfd67c3326a37905a0ad38
neethugino <73609206+neethugino@users.noreply.github.com>
2024-02-22 00:20:45 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from app import db 
 
+#Define User model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
